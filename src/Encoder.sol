8a1475e6c752c33d76d6b55077005a01f32884d2
James Wenzel <wenzel.james.r@gmail.com>
2023-10-17 12:16:27 -0700

    tests for encoder

--- src/Encoder.sol
+++ src/Encoder.sol
@@ -15,94 +15,123 @@ library Encoder {
     uint256 constant TYPE1_LENGTH_ADJUST = 1;
     uint256 constant TYPE2_LENGTH_ADJUST = 2;
     uint256 constant ONE_BYTE_BITS = 8;
+    uint256 constant EXPAND_FLAG = 0x20;
+    uint256 constant POINTER_FLAG = 0x40;
 
+    /**
+     * @notice Encodes a bytes32 value into a bytes array according to the "Type1" schema
+     *         Type1 is the concatenation of the following bytes: (VAL_WIDTH_BYTES - 1)<right-packed VAL>
+     * @param x The bytes32 value to encode
+     */
     function encodeType1(bytes32 x) internal pure returns (bytes memory encoded) {
-        // get number of bytes width for x
-        uint256 bitWidth = msb(uint256(x));
-        // range(0, 32]
-        uint256 byteWidth = (bitWidth >> BITS_TO_BYTES_SHIFT) + 1;
-        return _encodeType1(x, byteWidth);
+        (uint256 valWidth, uint256 lengthAdjust, uint256 meta, bytes32 newVal) = type1Components(x);
+        return encodeFromComponents(uint256(valWidth), uint256(lengthAdjust), uint256(meta), newVal);
     }
 
-    function _encodeType1(bytes32 x, uint256 byteWidth) private pure returns (bytes memory encoded) {
-        assembly {
-            // get pointer to encoded
-            encoded := mload(FREE_PTR)
-            // update free memory pointer by up to 3 words
-            // 1 for encoded length, 1 for first 32 bytes, optionally 1 for extra byte
-            // bytesToAllocate = (byteWidth == 32) ? 0x60 : 0x40
-            let bytesToAllocate := add(TWO_WORDS, and(byteWidth, ONE_WORD))
-            mstore(FREE_PTR, add(bytesToAllocate, encoded))
-
-            let bytesArrayLength := add(TYPE1_LENGTH_ADJUST, byteWidth)
-            // store x after length with one extra byte of padding for the
-            // meta var
-            mstore(add(encoded, bytesArrayLength), x)
-
-            // store the meta var in the byte immediately after the bytes array length
-            mstore(
-                add(encoded, TYPE1_META_OFFSET),
-                // (bytewidth == 0) ? 0 : bytewidth - 1
-                // bytewidth - 1
-                sub(byteWidth, 1)
-            )
-            // store array length
-            mstore(encoded, bytesArrayLength)
-        }
-    }
-
+    /**
+     * @notice Encodes a bytes32 value into a bytes array according to the "Type2" schema
+     *         Type1 is the concatenation of the following bytes: ((VAL_WIDTH_BYTES - 1) | EXPAND_FLAG?)(EXPAND_BITS? or NULL)<packed VAL>
+     * @param x The bytes32 value to encode
+     */
     function encodeType2(bytes32 x) internal pure returns (bytes memory encoded) {
-        uint256 expansionBits = lsb(uint256(x));
+        (uint256 valWidth, uint256 lengthAdjust, uint256 meta, bytes32 y) = type2Components(x);
+        return encodeFromComponents(uint256(valWidth), uint256(lengthAdjust), uint256(meta), y);
+    }
 
-        uint256 bitWidth = msb(uint256(x));
-        uint256 byteWidth = ((bitWidth - expansionBits) >> BITS_TO_BYTES_SHIFT + 1);
-        // don't encode expansion bits if it is more expensive than including all zero-bytes
-        // eg, 1 non-zero byte is as expensive as 4 zero-bytes
-        if (expansionBits < EXPANSION_BIT_CUTOFF) {
-            // recalculate byteWidth
-            byteWidth = (bitWidth + BITS_TO_BYTES_ROUND_UP) >> BITS_TO_BYTES_SHIFT;
-            return _encodeType1(x, byteWidth);
-        }
-        assembly {
-            let packedX := shr(expansionBits, x)
-
-            // get pointer to encoded
-            encoded := mload(FREE_PTR)
-            // update free memory pointer by 2 words
-            // 1 for encoded length, 1 for first 32 bytes, optionally 1 for extra byte
-            // bytesToAllocate = (byteWidth == 32) ? 0x60 : 0x40
-            let bytesToAllocate := add(TWO_WORDS, and(byteWidth, ONE_WORD))
-            mstore(FREE_PTR, add(bytesToAllocate, encoded))
-
-            let bytesArrayLength := add(TYPE2_LENGTH_ADJUST, byteWidth)
-            // store x after length with one extra byte of padding for the
-            // meta var
-            mstore(add(encoded, bytesArrayLength), packedX)
-            // store the expansion bits in the second byte immediately after the bytes array length
-            // store the meta var in the 2 bytes immediately after the bytes array length
-
-            mstore(
-                add(encoded, TYPE2_META_OFFSET),
-                // meta var is 2 bytes; first byte is (byteWidth - 1 or 0), second is expansionBits
-                or(
-                    // shift left by one byte
-                    shl(ONE_BYTE_BITS, sub(byteWidth, 1)),
-                    expansionBits
-                )
-            )
-            // store array length
-            mstore(encoded, bytesArrayLength)
+    /**
+     * @notice Returns the components of a Type1 encoding for a given value
+     * @param x The bytes32 value to encode
+     * @return valWidth
+     * @return metaWidth
+     * @return meta
+     * @return newVal
+     */
+    function type1Components(bytes32 x)
+        private
+        pure
+        returns (uint256 valWidth, uint256 metaWidth, uint256 meta, bytes32 newVal)
+    {
+        unchecked {
+            valWidth = (msb(uint256(x)) >> BITS_TO_BYTES_SHIFT) + 1;
+            metaWidth = 1;
+            meta = valWidth - 1;
+            newVal = x;
         }
     }
 
+    /**
+     * @notice Returns the components of a Type2 encoding for a given value
+     * @param x The bytes32 value to get encoding components for
+     * @return valWidth
+     * @return metaWidth
+     * @return meta
+     * @return newVal
+     */
+    function type2Components(bytes32 x)
+        internal
+        pure
+        returns (uint256 valWidth, uint256 metaWidth, uint256 meta, bytes32 newVal)
+    {
+        unchecked {
+            uint256 expansionBits = lsb(uint256(x));
+            if (expansionBits < EXPANSION_BIT_CUTOFF) {
+                return type1Components(x);
+            }
+            newVal = bytes32(uint256(x) >> expansionBits);
+            valWidth = (msb(uint256(newVal)) >> BITS_TO_BYTES_SHIFT) + 1;
+            metaWidth = 2;
+            meta = (((valWidth - 1) | EXPAND_FLAG) << ONE_BYTE_BITS) | expansionBits;
+        }
+    }
+
+    /**
+     *
+     * @param valWidth The width in bytes of the value to encode
+     * @param metaWidth The width in bytes of the encoding meta
+     * @param meta The encoding meta value
+     * @param y The bytes32 value to encode
+     */
+    function encodeFromComponents(uint256 valWidth, uint256 metaWidth, uint256 meta, bytes32 y)
+        internal
+        pure
+        returns (bytes memory encoded)
+    {
+        assembly {
+            // assign encoded to free memory pointer
+            encoded := mload(FREE_PTR)
+            let bytesToAllocate := add(TWO_WORDS, and(valWidth, ONE_WORD))
+            let byteArrayLength := add(metaWidth, valWidth)
+            // update free memory pointer by up to 3 words
+            mstore(FREE_PTR, add(bytesToAllocate, encoded))
+            // store y after length and meta
+            mstore(add(encoded, byteArrayLength), y)
+            // store the meta var in the byte(s) immediately after the bytes array length
+            mstore(add(encoded, metaWidth), meta)
+            // store array length
+            mstore(encoded, byteArrayLength)
+        }
+    }
+
+    /**
+     * @notice Finds the most significant bit of a uint256
+     * @param x Value to find the most significant bit of
+     */
     function msb(uint256 x) internal pure returns (uint256) {
+        // msb is equivalent of log2
         return FixedPointMathLib.log2(x);
     }
 
-    function lsb(uint256 x) internal pure returns (uint256 y) {
+    /**
+     * @notice Finds the least significant bit of a uint256
+     * @param x Value to find the least significant bit of
+     */
+    function lsb(uint256 x) internal pure returns (uint256) {
+        uint256 y;
         assembly {
+            // mask out all bits except the least significant bit
             y := and(x, sub(0, x))
         }
+        // get the position of the remaining bit
         return FixedPointMathLib.log2(y);
     }
 }
--- test/Encoder.t.sol
+++ test/Encoder.t.sol
@@ -51,7 +51,6 @@ contract EncoderTest is Test {
         }
         x = bytes32(y);
         encoded = Encoder.encodeType1(x);
-        emit log_bytes(encoded);
         assertEq(encoded.length, 33);
         assertEq(encoded[0], bytes1(0x1f));
         assertEq(encoded[1], bytes1(0xff));
@@ -64,6 +63,71 @@ contract EncoderTest is Test {
         assertEq(encoded.length, 2);
         assertEq(encoded[0], 0x00);
         assertEq(encoded[1], 0x00);
+
+        x = bytes32(bytes1(0x01));
+        encoded = Encoder.encodeType2(x);
+        assertEq(encoded.length, 3);
+        assertEq(encoded[0], bytes1(uint8(0x00 | Encoder.EXPAND_FLAG)));
+        assertEq(encoded[1], bytes1(uint8(248)));
+        assertEq(encoded[2], bytes1(0x01));
+
+        x = bytes32(bytes2(0x00ff));
+        encoded = Encoder.encodeType2(x);
+        assertEq(encoded.length, 3);
+        assertEq(encoded[0], bytes1(uint8(0x00 | Encoder.EXPAND_FLAG)));
+        assertEq(encoded[1], bytes1(uint8(240)));
+        assertEq(encoded[2], bytes1(0xff));
+        x = bytes32(uint256(1));
+        encoded = Encoder.encodeType2(x);
+        assertEq(encoded.length, 2);
+        assertEq(encoded[0], 0x00);
+        assertEq(encoded[1], bytes1(0x01));
+        x = bytes32(uint256(1 << 255) - 1);
+        encoded = Encoder.encodeType2(x);
+        assertEq(encoded.length, 33);
+        assertEq(encoded[0], bytes1(uint8(0x1f)));
+        assertEq(encoded[1], bytes1(0x7f));
+        assertEq(encoded[32], bytes1(0xff));
+
+        x = bytes32(bytes4(0x000001c0));
+        encoded = Encoder.encodeType2(x);
+        assertEq(encoded.length, 3);
+        assertEq(encoded[0], bytes1(uint8(0x00 | Encoder.EXPAND_FLAG)));
+        assertEq(encoded[1], bytes1(uint8(230)));
+        assertEq(encoded[2], bytes1(0x07));
+    }
+
+    function testEncodeType2SpanningBits() public {
+        uint256 x;
+        for (uint256 i = 4; i < 31; i++) {
+            uint256 bits = i * 8;
+            uint256 shift = bits + 7;
+            x = 3 << shift;
+            bytes memory encoded = Encoder.encodeType2(bytes32(x));
+            assertEq(encoded.length, 3);
+            assertEq(encoded[0], bytes1(uint8(0x00 | Encoder.EXPAND_FLAG)));
+            assertEq(encoded[1], bytes1(uint8(shift)));
+            assertEq(encoded[2], bytes1(0x03));
+        }
+    }
+
+    function testEncodePackingBits() public {
+        uint256 x;
+        for (uint256 i = 0; i < Encoder.EXPANSION_BIT_CUTOFF; i++) {
+            x = 1 << i;
+            bytes memory encoded = Encoder.encodeType2(bytes32(x));
+            assertEq(encoded.length, 2 + (i / 8), "bad length");
+            assertEq(encoded[0], bytes1(uint8(i / 8)), "bad meta");
+            assertEq(encoded[1], bytes1(uint8(1 << (i % 8))), "bad value");
+        }
+        for (uint256 i = Encoder.EXPANSION_BIT_CUTOFF; i < 256; i++) {
+            x = 1 << i;
+            bytes memory encoded = Encoder.encodeType2(bytes32(x));
+            assertEq(encoded.length, 3, "bad length");
+            assertEq(encoded[0], bytes1(uint8(0x00 | Encoder.EXPAND_FLAG)), "bad meta");
+            assertEq(encoded[1], bytes1(uint8(i)), "bad expand");
+            assertEq(encoded[2], bytes1(0x01), "bad val");
+        }
     }
 
     function testMsb() public {
